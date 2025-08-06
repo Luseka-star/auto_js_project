fe1aa623b8d0236b833bc5cbbe825d985b0bf1e0
Liudmyla Stambula <liudmylastambula@gmail.com>
2025-08-06 10:26:16 -0700

    added config files + made changes in test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,9 +6,9 @@ test('Verify login of existed user', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password'}).fill('welcome01');
   await page.getByRole('button', {name: 'Login'}).click();
 
-  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+  await expect(page).toHaveURL('/account');
   await expect(page.getByRole('heading', { name: 'My account'})).toContainText('My account');
   await expect(page.getByRole('button', { name: 'Jane Doe'})).toBeVisible();
 
 });
-пше 
+  
