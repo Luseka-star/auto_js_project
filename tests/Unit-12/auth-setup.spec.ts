b02519a3ffb334afbe50d266d0624d11910bf373
Liudmyla Stambula <liudmylastambula@gmail.com>
2025-09-15 11:45:23 -0700

    Unit-12 tests

--- tests/Unit-12/auth-setup.spec.ts
+++ tests/Unit-12/auth-setup.spec.ts
@@ -6,6 +6,8 @@ import path from 'path';
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
 test('Login via UI and save session', async ({ page }) => {
+    // test.skip(!!process.env.GITHUB_ACTIONS, 'Skip it in GitHub Actions');
+
   const loginPage = new LoginPage(page);
 
   await page.goto('/auth/login');
